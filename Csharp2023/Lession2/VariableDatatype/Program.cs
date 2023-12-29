0a63a6a190da1ead9d8386f72c15e3504792d434
ducql56 <longduc91193@gmail.com>
2023-12-29 23:18:17 +0700

    unit2 them

--- Csharp2023/Lession2/VariableDatatype/Program.cs
+++ Csharp2023/Lession2/VariableDatatype/Program.cs
@@ -1,2 +1,30 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+//string Name = "messi";
+//int Age = 36;
+//string name = "édfggh";
+//Console.WriteLine("Input in  name:");
+//string name = Console.ReadLine();
+//Console.WriteLine("Input in  Age:");
+//string age = Console.ReadLine();
+//Console.WriteLine($" name: {0} , age :{1}",name,age);
+//Console.WriteLine(string.Format("name: {0}, age: {1}",name,age));
+//Console.ReadKey();
+long number1 = 10;
+int number2 = 20;
+long number8 = number2;
+int numer10 = (int)number1;
+
+char ch = 'a';
+object number14 = number1;
+int number15 = (int)number14;
+DateTime today = DateTime.Now;
+
+
+
+
+
+
+double number3 = 6.8;
+float floatale = 8.7F;
+Console.ReadKey();
