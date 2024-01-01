d4628bffa9c76478d0ada6cd0e97470684c90011
ducql56 <longduc91193@gmail.com>
2024-01-01 21:49:24 +0700

    Lession3_HW

---
+++ Csharp2023/Lession3/Lession3HW/Program.cs
@@ -0,0 +1,16 @@
+﻿int a;
+Console.WriteLine("Input number a:");
+a = int.Parse(Console.ReadLine());
+if (a < 0)
+{
+    Console.WriteLine($"{0} is negative integers ", a);
+}
+if (a == 0)
+{
+    Console.WriteLine($"{0} is rezo", a);
+}
+if (a > 0)
+{
+    Console.WriteLine($"{0} is positive integer", a);
+}
+Console.ReadKey();
