223a240be1722b9a5ba0497989da6f96f7029488
ducql56 <longduc91193@gmail.com>
2024-01-10 19:53:07 +0700

    tu hoc

---
+++ Csharp2023/Lession3/HW3/HW3A.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW3
+{
+    public class HW3A
+    {
+        public void InputNumber()
+        {
+            Console.WriteLine(" Input Number:");
+
+            try
+            {
+                string value = Console.ReadLine();
+                int number = int.Parse(value);
+
+                if (number == 0)
+                {
+                    Console.WriteLine(" the  number is 0");
+                }
+                else if (number > 0)
+                {
+                    Console.WriteLine(" The number is posstive number");
+                }
+                else if (number < 0)
+                {
+                    Console.WriteLine(" the number is");
+                }
+            }
+            catch (Exception ex) {
+                //Console.WriteLine(" da co loi xay ra:");
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        public void InputNumber1()
+        {
+            Console.WriteLine(" Input Number:");
+
+            
+          
+             string value = Console.ReadLine();
+
+                
+            bool check = int.TryParse(value, out int number);
+
+            if (check == false)
+            {
+                Console.WriteLine("Number is not correct");
+                return;
+            }
+            if (number == 0)
+                {
+                    Console.WriteLine(" the  number is 0");
+                }
+                else if (number > 0)
+                {
+                    Console.WriteLine(" The number is posstive number");
+                }
+                else if (number < 0)
+                {
+                    Console.WriteLine(" the number is");
+                }
+            
+        }
+    }
+    
+}
---
+++ Csharp2023/Lession3/HW3/Program.cs
@@ -0,0 +1,6 @@
+﻿using HW3;
+
+HW3A hw = new HW3A();
+hw.InputNumber();
+hw.InputNumber1();
+Console.ReadKey();
