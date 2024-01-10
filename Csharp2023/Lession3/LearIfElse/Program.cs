122c64a1218752f7985dabf99823bf8e11bb0743
ducql56 <longduc91193@gmail.com>
2024-01-10 20:42:54 +0700

    finishlession3

--- Csharp2023/Lession3/LearIfElse/Program.cs
+++ Csharp2023/Lession3/LearIfElse/Program.cs
@@ -2,4 +2,8 @@
 
 sevice service = new sevice();
 string result = service.EvaluaStudent(5);
+bool check = service.CheckLunarYear(2026);
+bool check2 = service.CheckLunarYear1(2020);
+bool check3 = service.CheckLunarYear2(2020);
+bool check4 = service.CheckLunarYear3(2020);
 Console.ReadKey();
--- Csharp2023/Lession3/LearIfElse/sevice.cs
+++ Csharp2023/Lession3/LearIfElse/sevice.cs
@@ -25,5 +25,54 @@ namespace LearIfElse
 
             return result;
         }
+        public bool CheckLunarYear(int year)
+        {
+            if (year < 0)
+            {
+                return false;
+            }
+
+            if (year % 100 == 0)
+            {
+                if (year % 400 == 0)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            else
+            {
+                if (year % 4 == 0)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+        }
+
+
+        public bool CheckLunarYear1(int year)
+        {
+            if ((year % 100 == 0 && year % 400 == 0) || (year % 100 != 0 && year % 4 == 0))
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public bool CheckLunarYear2(int year)
+        {
+            return (year % 100 == 0 && year % 400 == 0) || (year % 100 != 0 && year % 4 == 0);
+        }
+
+        public bool CheckLunarYear3(int year)
+            => (year % 100 == 0 && year % 400 == 0) || (year % 100 != 0 && year % 4 == 0);
     }
+
 }
