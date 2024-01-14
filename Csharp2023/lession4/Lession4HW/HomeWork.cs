14684ba7653eb7becd337781915dde1d535019c4
ducql56 <longduc91193@gmail.com>
2024-01-14 22:39:04 +0700

    fisnish Homework

---
+++ Csharp2023/lession4/Lession4HW/HomeWork.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lession4HW
+{
+    public class HomeWork
+    {
+        public string Gettriangle(int edgeA, int edgeB, int edgeC)
+        {
+
+            string Messege = string.Empty;
+            if ((edgeA + edgeB < edgeC) || (edgeC + edgeB < edgeA) || (edgeC + edgeA < edgeB))
+            {
+                Messege = ("This is not Triangle");
+
+            }
+            else if ((edgeA == edgeB) || (edgeB == edgeC) || (edgeC == edgeA))
+            {
+                Messege = ("This is  isosceles triangle");
+
+            }
+            else if ((Math.Pow(edgeA, 2) + Math.Pow(edgeB, 2) == Math.Pow(edgeC, 2)) || (Math.Pow(edgeC, 2) + Math.Pow(edgeB, 2) == Math.Pow(edgeA, 2)) || (Math.Pow(edgeC, 2) + Math.Pow(edgeA, 2) == Math.Pow(edgeB, 2)))
+            {
+                Messege = ("This is right triangle");
+            }
+            else
+                Messege = "This is regular triangle";
+            return Messege;
+        }
+    }
+}
---
+++ Csharp2023/lession4/Lession4HW/Program.cs
@@ -0,0 +1,6 @@
+﻿using Lession4HW;
+
+HomeWork sevice = new HomeWork();
+String triangle = sevice.Gettriangle(1, 1, 10);
+
+Console.ReadKey();
