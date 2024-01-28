3fa42a1b667d8c83020222cff90ec78e1885e1fc
ducql56 <longduc91193@gmail.com>
2024-01-28 23:05:09 +0700

    finissh

--- Csharp2023/Lession_8/Lession8_HW/Student.cs
+++ Csharp2023/Lession_8/Lession8_HW/Student.cs
@@ -35,6 +35,8 @@ namespace Lession8_HW
                     Console.WriteLine(" Reinput your Age:");
                     checkAge = int.TryParse(Console.ReadLine(), out Age);
                 }
+                Console.WriteLine(" input the score: ");
+
                 bool checkScore = double.TryParse(Console.ReadLine(), out score);
                 while (!checkScore)
                 {
