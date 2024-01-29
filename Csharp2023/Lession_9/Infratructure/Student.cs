c907882518447dd3a0f2fff9d0275491b2a9fc83
ducql56 <longduc91193@gmail.com>
2024-01-29 19:48:17 +0700

    hi

---
+++ Csharp2023/Lession_9/Infratructure/Student.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infratructure
+{
+    public  class Student
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public double Math { get; set; }
+        public double Litteratue { get; set; }
+        public Student()
+        {
+            
+        }
+    }
+}
